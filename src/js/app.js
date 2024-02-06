40bfcea208dc37a4a9fb4015166c8126d4fbda96
Samantha <144262012+samanthacxxz@users.noreply.github.com>
2024-02-06 22:38:26 +0100

    last step - declare class for product item

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,31 @@
-console.log('test');
+
+
+// SELECTING ELEMENTS FROM THE DOM
+
+const medicineForm = document.querySelector('.medicine-form');
+
+const productName = document.querySelector('.product-name');
+const productID = document.querySelector('.product-id');
+const manufacturer = document.querySelector('.manufacturer');
+const expirationDate = document.querySelector('.expiration-date');
+const quantity = document.querySelector('.quantity');
+
+
+// ADDING EVENT LISTENERS
+// when submitting 
+
+// DECLARE PRODUCT CLASS
+
+class ProductItem {
+    constructor(productName, productID, manufacturer, expirationDate, quantity) {
+        this.productName = productName;
+        this.productID = productID;
+        this.manufacturer = manufacturer;
+        this.expirationDate = expirationDate;
+        this.quantity = quantity;
+    }
+}
+
+class ManufacturerPfizer extends ProductItem {
+
+}
