ac8b878aa4f94089506096c977495a1f5dde645b
Samantha <144262012+samanthacxxz@users.noreply.github.com>
2024-02-12 13:36:35 +0100

    pushing item to an specific array

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,7 @@
-
+const pfizerItems = [];
+const merckAndCoItems = [];
+const abbvieItems = [];
+const otherItems = [];
 
 // SELECTING ELEMENTS FROM THE DOM
 
@@ -10,9 +13,30 @@ const manufacturer = document.querySelector('.manufacturer');
 const expirationDate = document.querySelector('.expiration-date');
 const quantity = document.querySelector('.quantity');
 
+const submitButton = document.querySelector('.submit-button');
+
 
 // ADDING EVENT LISTENERS
-// when submitting 
+// when clicking "register medicine" button
+medicineForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let newItem;
+    if (manufacturer.value === 'pfizer') {
+        newItem =  new ProductItem (
+            productName.value,
+            productID.value,
+            manufacturer.value,
+            expirationDate.value,
+            quantity.value
+        )
+    } else if (manufacturer.value === 'merck-and-co'){
+
+    }
+   
+    ProductItem.addProductItem(newItem);
+    console.log(items);
+})
+
 
 // DECLARE PRODUCT CLASS
 
@@ -24,8 +48,24 @@ class ProductItem {
         this.expirationDate = expirationDate;
         this.quantity = quantity;
     }
-}
+    static addProductItem(item) {
+        if (ProductItem.manufacturer === 'pfizer') {
+            pfizerItems.push(item);
+        } else if (ProductItem.manufacturer === 'merck-and-co'){
+            merckAndCoItems.push(item);
+        } else if (ProductItem.manufacturer === 'abbvie') {
+            abbvieItems.push(item);
+        } else {
+            otherItems.push(item);
+        }
+    };
+};
 
-class ManufacturerPfizer extends ProductItem {
+
+
+
+// DECLARE UI CLASS 
+
+class UI {
 
 }
