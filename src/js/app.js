5bb8e7bb5db97e70f6f343fe40b1726e2e3c16d2
Samantha <144262012+samanthacxxz@users.noreply.github.com>
2024-02-13 12:38:41 +0100

    added class for manufacturer, and UI, and eventlistener for submit

--- src/js/app.js
+++ src/js/app.js
@@ -13,6 +13,11 @@ const manufacturer = document.querySelector('.manufacturer');
 const expirationDate = document.querySelector('.expiration-date');
 const quantity = document.querySelector('.quantity');
 
+const displayPfizerContainer = document.querySelector('.display-pfizer-container');
+const displayMerckAndCoContainer = document.querySelector('.display-merck-and-co-container');
+const displayAbbvieContainer = document.querySelector('.display-abbvie-container');
+const displayOtherContainer = document.querySelector('.display-other-container');
+
 const submitButton = document.querySelector('.submit-button');
 
 
@@ -21,26 +26,53 @@ const submitButton = document.querySelector('.submit-button');
 medicineForm.addEventListener('submit', (e) => {
     e.preventDefault();
     let newItem;
-    if (manufacturer.value === 'pfizer') {
-        newItem =  new ProductItem (
+    console.log(manufacturer.value);
+    if (manufacturer.value === "pfizer") {
+        newItem = new Item (
             productName.value,
             productID.value,
             manufacturer.value,
             expirationDate.value,
             quantity.value
         )
-    } else if (manufacturer.value === 'merck-and-co'){
-
+    } else if (manufacturer.value === "merck-and-co"){
+        newItem = new MerckAndCoItem (
+            productName.value,
+            productID.value,
+            manufacturer.value,
+            expirationDate.value,
+            quantity.value
+        )
+    } else if (manufacturer.value === "abbvie"){
+        newItem =  new AbbvieItem (
+            productName.value,
+            productID.value,
+            manufacturer.value,
+            expirationDate.value,
+            quantity.value
+        )
+    } else {
+        newItem =  new OtherItem (
+            productName.value,
+            productID.value,
+            manufacturer.value,
+            expirationDate.value,
+            quantity.value
+        )
     }
    
-    ProductItem.addProductItem(newItem);
-    console.log(items);
+    Item.addProductItem(newItem);
+    medicineForm.reset();
+
+    console.log(pfizerItems);
+    console.log(merckAndCoItems);
+    console.log(abbvieItems);
 })
 
 
 // DECLARE PRODUCT CLASS
 
-class ProductItem {
+class Item {
     constructor(productName, productID, manufacturer, expirationDate, quantity) {
         this.productName = productName;
         this.productID = productID;
@@ -49,11 +81,11 @@ class ProductItem {
         this.quantity = quantity;
     }
     static addProductItem(item) {
-        if (ProductItem.manufacturer === 'pfizer') {
+        if (item.manufacturer === 'pfizer') {
             pfizerItems.push(item);
-        } else if (ProductItem.manufacturer === 'merck-and-co'){
+        } else if (Item.manufacturer === 'merck-and-co'){
             merckAndCoItems.push(item);
-        } else if (ProductItem.manufacturer === 'abbvie') {
+        } else if (Item.manufacturer === 'abbvie') {
             abbvieItems.push(item);
         } else {
             otherItems.push(item);
@@ -61,13 +93,43 @@ class ProductItem {
     };
 };
 
-/*class pfizerItem {
-    constructor(productName, productID, manufacturer, expirationDate, quantity)
+class MerckAndCoItem extends Item  {
+    constructor(productName, productID, manufacturer, expirationDate, quantity){
         super(productName, productID, manufacturer, expirationDate, quantity);
-}*/
+    }
+
+}
+class AbbvieItem extends Item  {
+    constructor(productName, productID, manufacturer, expirationDate, quantity){
+        super(productName, productID, manufacturer, expirationDate, quantity);
+    }
+}
+class OtherItem extends Item  {
+    constructor(productName, productID, manufacturer, expirationDate, quantity){
+        super(productName, productID, manufacturer, expirationDate, quantity);
+    }
+}
 
 // DECLARE UI CLASS 
 
 class UI {
     static activeTab = '';
+    static renderPfizerItems() {
+        displayPfizerContainer.style.display = "block";
+        displayMerckAndCoContainer.style.display = "none";
+        displayAbbvieContainer.style.display = "none";
+        displayOtherContainer.style.display = "none";
+
+        if (UI.activeTab === 'pfizer') {
+            items.forEach((item) => {
+                const liRow = document.querySelector('li');
+                
+                const renderedProductName = document.querySelector('li');
+                const renderedProductID = document.querySelector('li');
+                const renderedManufacturer = document.querySelector('li');
+                const renderedExpirationDate = document.querySelector('li');
+                const renderedQuantity = document.querySelector('li');
+            })
+        }
+    }
 }
